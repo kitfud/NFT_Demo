f8e75cf8c267e2bada3b993c828a4978f58bdf19
Kit Fuderich <kitfuderich@gmail.com>
2022-01-17 11:39:33 -0800

    simple test created for deploy script

---
+++ README.md
@@ -0,0 +1,1 @@
+1. Token URI which already exists was used instead of actually uploading to IPFS for the demo
--- scripts/deploy_and_create.py
+++ scripts/deploy_and_create.py
@@ -6,7 +6,7 @@ OPENSEA_URL = "https://testnets.opensea.io/{}/{}"
 sample_token_uri = "https://ipfs.io/ipfs/Qmd9MCGtdVz2miNumBHDbvj8bigSgTwnr4SbyH6DNnpWdt?filename=0-PUG.json"
 
 
-def main():
+def deploy_and_create():
     account = get_account()
     simple_collectible = SimpleCollectible.deploy({"from": account})
     tx = simple_collectible.createCollectible(sample_token_uri, {"from": account})
@@ -14,3 +14,8 @@ def main():
     print(
         f"Awsome, NFT ready for viewing at {OPENSEA_URL.format(simple_collectible.address,simple_collectible.tokenCounter()-1)}"
     )
+    return simple_collectible
+
+
+def main():
+    deploy_and_create()
