9c1b5d4b93c88ae994fae3b0ad0dfb89bebbdf43
Kit Fuderich <kitfuderich@gmail.com>
2022-01-17 11:31:43 -0800

    collectible deployed using sample data from ipfs

--- contracts/SimpleCollectible.sol
+++ contracts/SimpleCollectible.sol
@@ -7,13 +7,17 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract SimpleCollectible is ERC721 {
     uint256 public tokenCounter;
 
-    constructor() public ERC721("MainNet", "MAN") {
+    constructor() public ERC721("Main Net", "MAN") {
         tokenCounter = 0;
     }
 
-    function createCollectible() public returns (uint256) {
+    function createCollectible(string memory tokenURI)
+        public
+        returns (uint256)
+    {
         uint256 newTokenId = tokenCounter;
         _safeMint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
         tokenCounter = tokenCounter + 1;
         return newTokenId;
     }
